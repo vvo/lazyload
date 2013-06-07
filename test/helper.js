0eb8703d015250b4f10ac36f8988f19f7a4aeae4
vvo <vincent.voyer@gmail.com>
2013-06-07 12:13:16 +0200

    Add test helper functions to get loaded/noLoaded test easily

--- test/helper.js
+++ test/helper.js
@@ -1,11 +1,11 @@
 (function(win, doc){
+  var defaultAttr = 'data-src'
 
   win.getImage = function getImage(params) {
-
     params.x = params.x || 0;
     params.y = params.y || 0;
     params.lzld = params.lzld || 'lzld';
-    params.lazyAttr = params.lazyAttr || 'data-src';
+    params.lazyAttr = params.lazyAttr || defaultAttr;
     params.id = params.id || 'i0';
     params.fakeSrc = params.fakeSrc ||
       'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
@@ -16,9 +16,32 @@
       'src="'+params.fakeSrc+'" ' +
       'style="position:relative;top: ' + params.y + 'px;left:' + params.x + 'px"' +
       params.lazyAttr + '="' + params.realSrc + '" ' +
+      'width=1 height=1 ' +
       'onload="' + params.lzld + '(this)" ' +
       'onerror="' + params.lzld + '(this)" ' +
       'id="' + params.id + '" />';
+  }
+
+  win.eltNotLoaded = function eltNotLoaded(eltId, lazyAttr) {
+    lazyAttr = lazyAttr || defaultAttr;
+
+    return function(done) {
+      setTimeout(function() {
+        assert(document.getElementById(eltId).getAttribute(lazyAttr) !== null);
+        done();
+      }, 25);
+    }
+  }
+
+  win.eltLoaded = function eltLoaded(eltId, lazyAttr) {
+    lazyAttr = lazyAttr || defaultAttr;
+
+    return function(done) {
+      setTimeout(function() {
+        assert(document.getElementById(eltId).getAttribute(lazyAttr) === null);
+        done();
+      }, 25);
+    }
   }
 
   var oldClean = win['clean'];
--- test/simple.js
+++ test/simple.js
@@ -2,17 +2,17 @@ describe('a simple usage with an image', function() {
   var fakeSrc = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
   var realSrc = 'fixtures/tiny.gif?'+(+new Date());
   var elt;
-  var playground = getPlayground();
 
   before(function() {
-    insert(playground, getImage({
+    insert(getPlayground(), getImage({
       x: 0,
       y: 0,
       realSrc: realSrc,
       fakeSrc: fakeSrc
     }));
+
     elt = document.getElementById('i0');
-  });
+  })
 
   it('src currently fake', function() {
     assert.equal(fakeSrc, elt.src);
@@ -22,12 +22,7 @@ describe('a simple usage with an image', function() {
     assert.equal(realSrc, elt.getAttribute('src'));
   });
 
-  it('loads the image when visible for a while', function(done) {
-    setTimeout(function() {
-      assert(elt.src.indexOf(realSrc) !== -1);
-      done();
-    }, 12);
-  });
+  it('loads the image when visible for a while', eltLoaded('i0'));
 
   after(clean);
 });
